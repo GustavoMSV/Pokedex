6489679986527cbccde72455a61811b9c1dc78f2
Gustavo Moreno <gustavomorenosiqueira86@gmail.com>
2022-08-04 01:21:03 -0300

    Finalizado Consts Js

--- js/script.js
+++ js/script.js
@@ -4,26 +4,49 @@ const pokemonImage = document.querySelector('.pokemon__image');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input__search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
-    return data;
+
+    if (APIResponse.status === 200){
+        const data = await APIResponse.json();
+        return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
-
+    pokemonName.innerHTML = 'Carregando...';
+    pokemonNumber.innerHTML = '';
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data ['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    if (data){
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data ['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value='';
+    }else {
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML = 'Não encontrado :('
+        pokemonNumber.innerHTML = '';
+    }
 }
 
 form.addEventListener('submit', (event) => {
-
     event.preventDefault();
-    
-    console.log('enviando formulario...')
-
+    renderPokemon(input.value.toLowerCase());
 });
+
+buttonPrev.addEventListener('click', () => {
+    alert('Anterior clicado');
+});
+
+buttonNext.addEventListener('click', () => {
+    alert('Próximo clicado');
+});
+
+renderPokemon(searchPokemon);
